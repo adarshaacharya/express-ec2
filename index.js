c1886e8df400f2c4fb825eda374b29601f35d10c
Aadarsha Acharya <connectwithadarsha@gmail.com>
2024-06-30 11:11:39 +0545

    first commit

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/public/index.html");
+});
+
+app.get("/about", (req, res) => {
+  res
+    .json({
+      message: "Hello from the about page!",
+    })
+    .status(200);
+});
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
